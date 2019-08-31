8e23fe5e0afe059611ee5a5888385009ac7a28c8
Kooku <koomg9599@gmail.com>
2019-08-31 19:53:17 +0900

    Install react-router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,24 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import './App.css'
 import Navigation from './components/navigation'
 import Footer from './components/footer'
 import Home from './containers/home'
-import NotFound from './containers/NotFound'
+import NotFound from './containers/not-found'
 
 function App() {
   return (
     <div className="App w-100 h-100">
       <div className="d-flex w-100 h-100 p-3 mx-auto flex-column">
-        <Navigation />
-        <Switch>
-          <Route path="/" component={Home} />
-          <Route component={NotFound} />
-          <Home />
-        </Switch>
-        <Footer />
+        <BrowserRouter>
+          <Navigation />
+          <Switch>
+            <Route path="/" component={Home} />
+            <Route component={NotFound} />
+            <Home />
+          </Switch>
+          <Footer />
+        </BrowserRouter>
       </div>
     </div>
   )
