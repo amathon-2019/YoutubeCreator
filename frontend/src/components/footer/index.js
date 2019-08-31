9616ceeee45a629288a652a6280340667706e72a
Kooku <koomg9599@gmail.com>
2019-08-31 16:00:36 +0900

    Fix footer message

--- frontend/src/components/footer/index.js
+++ frontend/src/components/footer/index.js
@@ -4,11 +4,11 @@ const Footer = () => {
   return (
     <footer className="mt-auto text-white-50">
       <p>
-        Site for
+        Site for{' '}
         <a href="https://youtub.com/" className="text-white">
           Youtube Creator
         </a>
-        , by
+        , by{' '}
         <a href="https://github.com/amathon-2019/YoutubeCreator" className="text-white">
           Team 7
         </a>
