86000b6c64b62f08ae8c19c36bf75248ac3bb300
Kooku <koomg9599@gmail.com>
2019-09-01 04:05:06 +0900

    Create get access token function

--- frontend/src/containers/login/index.js
+++ frontend/src/containers/login/index.js
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { getAccessToken } from '../../apis/access-token'
 
-const Login = () => {
+const Login = ({ location, history }) => {
+  async function getToken() {
+    const query = queryString.parse(location.search)
+    if (isEmpty(query)) history.push('/')
+    const response = await getAccessToken(query.code)
+    if (response) {
+      console.log(response)
+      history.push('/')
+    } else {
+      history.push('/')
+    }
+  }
+  useEffect(() => {
+    getToken()
+  })
   return <div>Loading...</div>
 }
 
