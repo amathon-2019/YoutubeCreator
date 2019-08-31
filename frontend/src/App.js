5fb26ff9b2fdaf6be95eca3406aa5056aead14dc
Kooku <koomg9599@gmail.com>
2019-08-31 15:37:58 +0900

    Create navigation component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import Navigation from './components/navigation'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+        <Navigation />
+      </div>
+    </div>
+  )
 }
 
 export default App
---
+++ frontend/src/components/navigation/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <header class="masthead mb-auto">
+      <div class="inner">
+        <h3 class="masthead-brand">Cover</h3>
+        <nav class="nav nav-masthead justify-content-center">
+          <a class="nav-link active" href="#">
+            Home
+          </a>
+          <a class="nav-link" href="#">
+            Features
+          </a>
+          <a class="nav-link" href="#">
+            Contact
+          </a>
+        </nav>
+      </div>
+    </header>
+  )
+}
+
+export default Navigation
