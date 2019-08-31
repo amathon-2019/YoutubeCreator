e8cb89c413338895aa55ccb7c732bc7ddfd30a4a
Kooku <koomg9599@gmail.com>
2019-08-31 15:56:33 +0900

    Create footer component

---
+++ frontend/src/components/footer/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="mt-auto text-white-50">
+      <p>
+        Site for
+        <a href="https://youtub.com/" className="text-white">
+          Youtube Creator
+        </a>
+        , by
+        <a href="https://github.com/amathon-2019/YoutubeCreator" className="text-white">
+          Team 7
+        </a>
+        .
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
